d8d04066a853155f7da754cef331bd24090d25cd
marton <marton12050@gmail.com>
2022-12-10 12:05:46 +0100

    Refacto and update hyperparameters

--- agents.py
+++ agents.py
@@ -5,12 +5,12 @@ import ray
 
 def impala(name, num_gpus, num_workers, max_steps, restore_chk):
         trainer_history = ray.tune.run(
-                ImpalaTrainer,
+                ImpalaTrainer,#Set agent algorithm
                 name=name,
                 config={
                         "env": "TTenv",
                         "framework": "torch",
-
+                        #Env specific config
                         "model": {
                                 "fcnet_activation": "relu"
                         },
@@ -19,13 +19,20 @@ def impala(name, num_gpus, num_workers, max_steps, restore_chk):
                                 "accepted_start_angle_deg": 5,
                         },
 
+                        #Agent specific config
                         "num_workers": num_workers,
                         "num_gpus": num_gpus,
-                        "train_batch_size": 1024, #Bigger batch size and my laptop crash lmao
-                        "gamma": 0.99,
-                        # If learning rate is too big, it will crash after around 100K steps(lr=0.0001)
-                        "lr": ray.tune.loguniform(0.00005, 5e-6),
-                        "seed":123,
+                        "train_batch_size": ray.tune.choice([1500, 2000]),
+                        "decay": ray.tune.loguniform(0.99, 0.995),
+                        "grad_clip":40,
+                        "epsilon": ray.tune.loguniform(0.1, 0.14),
+                        "gamma": 0.99,  
+                        # Always die after a few hundrew step,smaller learning rate a little bit longer
+                        # (probably env or ray problem from what I saw in the github issues)
+                        "lr": ray.tune.loguniform(0.0001, 5e-6),
+                        "sgd_minibatch_size": ray.tune.choice([150, 200]),
+                        "num_sgd_iter": ray.tune.choice([2, 4, 8, 16]),
+                        "lambda": 0.95,
                 },
                 stop={'timesteps_total': max_steps},
                 restore=restore_chk,
@@ -43,26 +50,32 @@ def impala(name, num_gpus, num_workers, max_steps, restore_chk):
 
 def ppo(name, num_gpus, num_workers, max_steps, restore_chk):
         trainer_history = ray.tune.run(
-                PPOTrainer,
+                PPOTrainer,#Set agent algorithm
                 name=name,
                 config={
                         "env": "TTenv",
                         "framework": "torch",
 
+                        #Env specific config
                         "model": {
                                 "fcnet_activation": "relu"
                         },
-
                         "env_config": {
                                 "accepted_start_angle_deg": 5,
                         },
+
+                        #Agent specific config
                         "num_workers": num_workers,
                         "num_gpus": num_gpus,
-                        "train_batch_size": 1024,
-                        "gamma": 0.99,  
+                        "train_batch_size": ray.tune.choice([1500, 2000]), #at 4096 my laptop crashing
                         "lr": ray.tune.loguniform(0.0001, 5e-6),
-                        "sgd_minibatch_size": 128,
-                        "lambda": 0.95,
+                        "clip_param": ray.tune.loguniform(0.28, 0.32),
+                        "gamma": 0.99,  
+                        "sgd_minibatch_size": ray.tune.choice([150, 200]),
+                        #"num_sgd_iter": random.choice([1, 2, 4, 8, 16]),
+                        "lambda": 0.98,
+                        "kl_target": ray.tune.loguniform(0.01, 0.015),
+                        "rollout_fragment_length": 200,
                 },
                 stop={'timesteps_total': max_steps},
                 restore=restore_chk,
